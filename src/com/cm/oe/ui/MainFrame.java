76d216be726633c0ed99d8a50cf4d257008ec532
admin <admin@192.168.3.49>
2016-01-15 17:54:14 +0800

    ui

--- src/com/cm/oe/ui/MainFrame.java
+++ src/com/cm/oe/ui/MainFrame.java
@@ -13,7 +13,7 @@ public class MainFrame extends JFrame {
 
 	/**
 	 * Create the frame.
-	 */
+	 ***/
 	public MainFrame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
