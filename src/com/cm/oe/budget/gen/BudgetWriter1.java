f28455a2ca035f3c131612dea4611a9c2230f541
yLance <83533450@163.com>
2016-01-22 16:33:48 +0800

    a

--- src/com/cm/oe/budget/gen/BudgetWriter1.java
+++ src/com/cm/oe/budget/gen/BudgetWriter1.java
@@ -1,11 +1,14 @@
 package com.cm.oe.budget.gen;
 
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -19,6 +22,7 @@ import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.openxml4j.opc.OPCPackage;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.streaming.SXSSFFormulaEvaluator.RowFlushedException;
 import org.apache.poi.xwpf.usermodel.XWPFDocument;
 import org.apache.poi.xwpf.usermodel.XWPFParagraph;
 import org.apache.poi.xwpf.usermodel.XWPFRun;
@@ -33,12 +37,12 @@ import org.openxmlformats.schemas.wordprocessingml.x2006.main.STBorder;
 import org.openxmlformats.schemas.wordprocessingml.x2006.main.STJc;
 import org.openxmlformats.schemas.wordprocessingml.x2006.main.STMerge;
 import org.openxmlformats.schemas.wordprocessingml.x2006.main.STVerticalJc;
+import org.w3c.dom.css.ElementCSSInlineStyle;
 
 import com.cm.oe.test.ReadExcel;
 import com.cm.oe.test.ReadExcelTable;
 
 public class BudgetWriter1 {
-	private String template = "";
 	private String path1 = "testfiles/ysb_final.xls";
 	private String path2 = "testfiles/3G4G工程基站预算基础信息表.xls";
 	private String tablePath = "testfiles/tables.xls";
@@ -50,21 +54,29 @@ public class BudgetWriter1 {
 	private ReadExcelTable ret = new ReadExcelTable();
 	private ReadExcel re = new ReadExcel();
 
-	public BudgetWriter1(String path1, String path2, String template, String tablePath, String excelPath,
+	public BudgetWriter1(String path1, String path2,  String tablePath, String excelPath,
 			String output) {
-		this.template = template;
 		this.path1 = path1;
 		this.path2 = path2;
 		this.tablePath = tablePath;
 		this.excelPath = excelPath;
 		this.output = output;
 	}
-
+	public static void main(String[] args) throws Exception {
+		 String path1 = "testfiles/ysb_final.xls";
+		 String path2 = "testfiles/3G4G工程基站预算基础信息表.xls";
+		 String tablePath = "testfiles/tables.xls";
+		 String excelPath = "C:\\Users\\王宁\\Desktop\\新建文件夹\\宏基站-室外站-D频段-上海贝尔-24111.xls";
+		 String output = "C:\\Users\\王宁\\Desktop\\新建文件夹\\";
+		BudgetWriter1 bw = new BudgetWriter1(path1, path2,  tablePath, excelPath, output);
+		bw.testReadByDoc();
+	}
 	public void testReadByDoc() throws Exception {
 		Map<Integer, List<String>> excelmap = new HashMap<Integer, List<String>>();
 		Map<Integer, List<String>> BBUtablemap = ret.readBBUinExcel(tablePath, excelPath);
 		Map<Integer, List<String>> RRUtablemap = ret.readRRUinExcel(tablePath, excelPath);
 		Map<Integer, List<String>> Antennatablemap = ret.readAntennaIntables(tablePath, excelPath);
+		Map<Integer, String> AZHoLa = ret.anzhuang(tablePath, excelPath);
 		Map<String, Map<String, String>> data_all = ub.get3G4Gjcxx(path2);
 		Map<String, List<String>> data_b3 = ub.getB3(path1);
 		Map<String, List<String>> datas_map = ub.getB3JData(data_all, data_b3, path1, path2);
@@ -75,27 +87,83 @@ public class BudgetWriter1 {
 		int rowNums = re.rowNumber(wb);
 		FileOutputStream fos = null;
 		Row r = null;
-		String name = null;
+		String city = null;
 
 		for (int i = 0; i < rowNums; i++) {
+			List<String> line = new ArrayList<String>();
 			r = sheet.getRow(i);
-			excelmap.put(i, re.getExcelvalues(r));
+			String zmzh = re.getExcelvalues(r).get(1).trim() + re.getExcelvalues(r).get(2).trim();
+			String dizhi = re.getExcelvalues(r).get(0).trim();
+			line.add(dizhi);
+			line.add(zmzh);
+			for(int j =3;j<re.getExcelvalues(r).size();j++){
+				line.add(re.getExcelvalues(r).get(j).trim());
+			}
+			excelmap.put(i, line);
 		}
 		//System.out.println(excelmap);
 		//System.out.println(RRUtablemap);
 		for (int i = 1; i < excelmap.size(); i++) {
-			
-			/**
-			 * 修改word文档中的表格内容
-			 */
-			InputStream is = new FileInputStream(template);
+			File file = new File(excelPath);
+			InputStream is = null ;
+			String[] strs = file.getName().split("-");
+			String  name =strs[0];
+			String neiwai = strs[1];
+			String pinduan = strs[2];
+			String shebei = strs[3];
+			if(name.trim().equals("宏基站")){
+				if(neiwai.trim().equals("室内站")){
+					is = new FileInputStream("testfiles/宏基站室内.docx");
+				}else if(neiwai.trim().equals("室外站")){
+					is = new FileInputStream("testfiles/宏基站室外.docx");
+				}
+				
+			}else if(name.trim().equals("小基站")){
+				if(neiwai.trim().equals("室内站")){
+					is = new FileInputStream("testfiles/小基站室内.docx");
+				}else if(neiwai.trim().equals("室外站")){
+					is = new FileInputStream("testfiles/小基站室外.docx");
+				}
+			}else if(name.trim().equals("拉远站")){
+				if(neiwai.trim().equals("室内站")){
+					is = new FileInputStream("testfiles/拉远站室内.docx");
+				}else if(neiwai.trim().equals("室外站")){
+					is = new FileInputStream("testfiles/拉远站室外.docx");
+				}
+			}else if(name.trim().equals("信源站")){
+				if(neiwai.trim().equals("室内站")){
+					is = new FileInputStream("testfiles/信源站室内.docx");
+				}else if(neiwai.trim().equals("室外站")){
+					is = new FileInputStream("testfiles/信源站室外.docx");
+				}
+			}
 			XWPFDocument doc = new XWPFDocument(is);
 			List<XWPFTable> tables = doc.getTables();
-			//System.out.println(excelmap.get(0).get(16).toString());
-			System.out.println(excelmap.get(0).get(16));
-			System.out.println(excelmap.get(0).get(16).toString().trim().equals("BBU型号"));
-			if (excelmap.get(0).get(16).toString().trim().equals("BBU型号")) {
-				XWPFTable tableBBU = tables.get(3);
+			if(name.trim().equals("信源站")){
+				XWPFTable table0 = tables.get(0);
+				setCellText(table0.getRow(0).getCell(1),excelmap.get(i).get(26).toString().trim(), "FFFFFF", 21);
+				setCellText(table0.getRow(1).getCell(1),pinduan.toString().trim(), "FFFFFF", 21);
+				setCellText(table0.getRow(2).getCell(1),name.toString().trim(), "FFFFFF", 21);
+				setCellText(table0.getRow(3).getCell(1),excelmap.get(i).get(27).toString().trim(), "FFFFFF", 21);
+				setCellText(table0.getRow(4).getCell(1),excelmap.get(i).get(28).toString().trim(), "FFFFFF", 21);
+			}else if(name.trim().equals("小基站")||name.trim().equals("拉远站")||name.trim().equals("宏基站")){
+				XWPFTable table0 = tables.get(0);
+				//TODO excelmap里的顺序需要重新改过
+				setCellText(table0.getRow(0).getCell(1), excelmap.get(i).get(26).toString().trim(), "FFFFFF", 21);
+				setCellText(table0.getRow(1).getCell(1), pinduan.toString().trim(), "FFFFFF", 21);
+				setCellText(table0.getRow(2).getCell(1), name.toString().trim(), "FFFFFF", 21);
+				setCellText(table0.getRow(3).getCell(1), excelmap.get(i).get(31).toString().trim(), "FFFFFF", 21);
+				setCellText(table0.getRow(4).getCell(1), excelmap.get(i).get(32).toString().trim(), "FFFFFF", 21);
+				setCellText(table0.getRow(0).getCell(3), shebei.toString().trim(), "FFFFFF", 21);
+				setCellText(table0.getRow(1).getCell(3), excelmap.get(i).get(27).toString().trim(), "FFFFFF", 21);
+				setCellText(table0.getRow(2).getCell(3), excelmap.get(i).get(28).toString().trim(), "FFFFFF", 21);
+				setCellText(table0.getRow(3).getCell(3), excelmap.get(i).get(29).toString().trim(), "FFFFFF", 21);
+				setCellText(table0.getRow(4).getCell(3), excelmap.get(i).get(30).toString().trim(), "FFFFFF", 21);
+			}
+			System.out.println(excelmap.get(0).get(18).toString());
+			System.out.println(excelmap.get(0).get(18).toString().trim().equals("BBU型号"));
+			if (excelmap.get(0).get(19).toString().trim().equals("BBU型号")) {
+				XWPFTable tableBBU = tables.get(2);
 				XWPFTableRow tBBURow = tableBBU.createRow();
 				tBBURow.setHeight(11);
 				//System.out.println(BBUtablemap);
@@ -103,10 +171,8 @@ public class BudgetWriter1 {
 					setCellText(tBBURow.getCell(j), BBUtablemap.get(i).get(j), "FFFFFF", 21);
 				}
 			}
-			System.out.println(excelmap.get(0).get(18).toString());
-			System.out.println(excelmap.get(0).get(18).toString().trim().equals("RRU型号"));
-			if (excelmap.get(0).get(18).toString().trim().equals("RRU型号")) {
-				XWPFTable tableRRU = tables.get(4);
+			if (excelmap.get(0).get(21).toString().trim().equals("RRU型号")) {
+				XWPFTable tableRRU = tables.get(3);
 				XWPFTableRow row = tableRRU.getRow(0);
 				//System.out.println(RRUtablemap.get(4));
 				if (RRUtablemap.get(i).get(8).toString().equals("工作频带宽度 ")) {
@@ -144,11 +210,9 @@ public class BudgetWriter1 {
 					setCellText(tRRURow.getCell(9), RRUtablemap.get(i).get(7), "FFFFFF", 21);
 
 				}
-			}
-			System.out.println(excelmap.get(0).get(20).toString());
-			System.out.println(excelmap.get(0).get(20).toString().trim().equals("天线型号"));
-				if (excelmap.get(0).get(20).toString().trim().equals("天线型号")) {
-					XWPFTable tableAnn = tables.get(5);
+			}	
+				if (excelmap.get(0).get(22).toString().trim().equals("天线型号")) {
+					XWPFTable tableAnn = tables.get(4);
 					XWPFTableRow tAnnRow = tableAnn.createRow();
 					tAnnRow.setHeight(11);
 					for (int j = 0; j < Antennatablemap.get(i).size(); j++) {
@@ -156,7 +220,7 @@ public class BudgetWriter1 {
 					}
 				}
 				
-				XWPFTable table = tables.get(2);
+				XWPFTable table = tables.get(1);
 				CTTblBorders borders = table.getCTTbl().getTblPr().addNewTblBorders();
 				genBorders(borders);
 				for (String key : datas_map.keySet()) {
@@ -169,7 +233,11 @@ public class BudgetWriter1 {
 
 				String zh = ub2.getZhFrom4GYsb();
 				List<String> datas = ub2.readExcel(zh);
-				table = tables.get(7);
+				if(name.toString().equals("信源站")||name.toString().equals("拉远站")){
+					table = tables.get(5);
+				}else{
+					table = tables.get(6);
+				}
 				borders = table.getCTTbl().getTblPr().addNewTblBorders();
 				genBorders(borders);
 				XWPFTableRow tableOneRowTwo = table.createRow();
@@ -181,7 +249,11 @@ public class BudgetWriter1 {
 				}
 
 				datas = ub3.readExcel(zh);
-				table = tables.get(8);
+				if(name.toString().trim().equals("信源站")||name.toString().trim().equals("拉远站")){
+					table = tables.get(6);
+				}else {
+					table = tables.get(7);
+				}
 				borders = table.getCTTbl().getTblPr().addNewTblBorders();
 				genBorders(borders);
 				tableOneRowTwo = table.createRow();
@@ -191,12 +263,10 @@ public class BudgetWriter1 {
 					setCellText(tableOneRowTwo.getCell(k), values, "FFFFFF", 21);
 					k++;
 				}
-				/**
-				 * 替换段落中的数据
-				 */
+
 				
-				/*OPCPackage pack = POIXMLDocument.openPackage(template);
-				XWPFDocument doc = new XWPFDocument(pack);*/
+				//OPCPackage pack = POIXMLDocument.openPackage(template);
+				//XWPFDocument doc = new XWPFDocument(pack);
 				List<XWPFParagraph> paragraphs = doc.getParagraphs();
 				//System.out.println(paragraphs.size());
 				for (XWPFParagraph tmp : paragraphs) {
@@ -206,46 +276,82 @@ public class BudgetWriter1 {
 						//System.out.println("XWPFRun-Text:" + aa.getText(0));
 						if ("city".equals(aa.getText(0))) {
 							aa.setText(excelmap.get(i).get(0), 0);
-						}else if("option2".equals(aa.getText(0))){
+						}
+						if("option2".equals(aa.getText(0))){
 							aa.setText(excelmap.get(i).get(1),0);
-						}else if("option3".equals(aa.getText(0))){
+						}
+						if("option3".equals(aa.getText(0))){
 							aa.setText(excelmap.get(i).get(2),0);
-						}else if("option4".equals(aa.getText(0))){
+						}
+						if("option4".equals(aa.getText(0))){
 							aa.setText(excelmap.get(i).get(3),0);
-						}else if("option5".equals(aa.getText(0))){
+						}
+						if("option5".equals(aa.getText(0))){
 							aa.setText(excelmap.get(i).get(4),0);
-						}else if("option6".equals(aa.getText(0))){
+						}
+						if("option6".equals(aa.getText(0))){
 							aa.setText(excelmap.get(i).get(5),0);
-						}else if("option7".equals(aa.getText(0))){
+						}
+						if("option7".equals(aa.getText(0))){
 							aa.setText(excelmap.get(i).get(6),0);
-						}else if("option8".equals(aa.getText(0))){
+						}
+						if("option8".equals(aa.getText(0))){
 							aa.setText(excelmap.get(i).get(7),0);
-						}else if("option9".equals(aa.getText(0))){
+						}
+						if("option9".equals(aa.getText(0))){
 							aa.setText(excelmap.get(i).get(8),0);
-						}else if("option10".equals(aa.getText(0))){
+						}
+						if("option10".equals(aa.getText(0))){
 							aa.setText(excelmap.get(i).get(9),0);
-						}else if("option11".equals(aa.getText(0))){
+						}
+						if("option11".equals(aa.getText(0))){
 							aa.setText(excelmap.get(i).get(10),0);
-						}else if("option12".equals(aa.getText(0))){
+						}
+						if("option12".equals(aa.getText(0))){
 							aa.setText(excelmap.get(i).get(11),0);
-						}else if("option13".equals(aa.getText(0))){
+						}
+						if("option13".equals(aa.getText(0))){
 							aa.setText(excelmap.get(i).get(12),0);
-						}else if("option14".equals(aa.getText(0))){
+						}
+						if("option14".equals(aa.getText(0))){
 							aa.setText(excelmap.get(i).get(13),0);
-						}else if("option15".equals(aa.getText(0))){
+						}
+						if("option15".equals(aa.getText(0))){
 							aa.setText(excelmap.get(i).get(14),0);
-						}else if("option16".equals(aa.getText(0))){
-							aa.setText(excelmap.get(i).get(15),0);
-						}else if("option17".equals(aa.getText(0))){
-							aa.setText(excelmap.get(i).get(17),0);
-						}else if("option18".equals(aa.getText(0))){
+						}
+						if("option16".equals(aa.getText(0))){
 							aa.setText(excelmap.get(i).get(19),0);
-						}else if("option19".equals(aa.getText(0))){
+						}
+						if("option17".equals(aa.getText(0))){
 							aa.setText(excelmap.get(i).get(21),0);
+						}
+						if("option18".equals(aa.getText(0))){
+							aa.setText(excelmap.get(i).get(22),0);
+						}
+						if("option19".equals(aa.getText(0))){
+							aa.setText(excelmap.get(i).get(21),0);
+						}
+						if("hongjizhan".equals(aa.getText(0))){
+							aa.setText(excelmap.get(i).get(13),0);
+						}
+						if("xiaojizhan".equals(aa.getText(0))){
+							aa.setText(excelmap.get(i).get(14),0);
+						}
+						if("layuanzhan".equals(aa.getText(0))){
+							aa.setText(excelmap.get(i).get(15),0);
+						}
+						if("xinyuanzhan".equals(aa.getText(0))){
+							aa.setText(excelmap.get(i).get(16),0);
+						}
+						//System.out.println(excelmap.get(i).get(25).toString().trim().equals("挂墙机框"));
+						//System.out.println(ret.anzhuangHongandLa(tablePath, excelPath));
+						//System.out.println(AZHoLa.get(0));
+						if("anzhuangfangshi".equals(aa.getText(0))){
+							aa.setText(AZHoLa.get(i),0);
 						}
 					}
 				name = excelmap.get(i).get(0);
-				fos = new FileOutputStream(output + name + ".doc");
+				fos = new FileOutputStream(output + city + ".doc");
 				doc.write(fos);
 				fos.flush();
 				fos.close();
@@ -253,8 +359,9 @@ public class BudgetWriter1 {
 			}
 			wb.close();
 			fise.close();
-		}	
+		}
 	}
+
 
 	private void genBorders(CTTblBorders borders) {
 		CTBorder hBorder = borders.addNewInsideH();
@@ -299,11 +406,7 @@ public class BudgetWriter1 {
 		cellR.setText(text);
 	}
 
-	/**
-	 * 关闭输入流
-	 * 
-	 * @param is
-	 */
+
 	private void close(InputStream is) {
 		if (is != null) {
 			try {
